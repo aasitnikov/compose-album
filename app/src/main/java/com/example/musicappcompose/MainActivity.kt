31a39e375654b3ce9b5d81ca7737a9be3b2d503c
Alexander Sitnikov <aasitnikov@yandex-team.ru>
2021-04-11 21:34:59 +0300

    add TrackRow

--- app/src/main/java/com/example/musicappcompose/MainActivity.kt
+++ app/src/main/java/com/example/musicappcompose/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.musicappcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Modifier
+import com.example.musicappcompose.ui.Track
+import com.example.musicappcompose.ui.TrackRow
 import com.example.musicappcompose.ui.theme.MusicAppComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,9 +20,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MusicAppComposeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    AlbumScreen(Overgrown)
                 }
             }
         }
@@ -26,14 +29,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MusicAppComposeTheme {
-        Greeting("Android")
+fun AlbumScreen(album: Album) {
+    LazyColumn(Modifier.fillMaxSize()) {
+        items(album.tracks) { track ->
+            TrackRow(track)
+        }
     }
 }
+
+data class Album(
+    val tracks: List<Track>,
+)
+
+val Overgrown = Album(
+    listOf(
+        Track("Overgrown", 1, false),
+        Track("I Am Sold", 2, false),
+        Track("Life Round Here", 3, true),
+        Track("Take A Fall For Me", 4, false),
+        Track("Retrograde", 5, true),
+        Track("DLM", 6, false),
+        Track("Digital Lion", 7, false),
+        Track("Voyeur", 8, false),
+        Track("To The Last", 9, false),
+        Track("Our Love Comes Back", 10, false),
+    )
+)
---
+++ app/src/main/java/com/example/musicappcompose/ui/Track.kt
@@ -0,0 +1,100 @@
+package com.example.musicappcompose.ui
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.musicappcompose.R
+import com.example.musicappcompose.ui.theme.MusicAppComposeTheme
+
+data class Track(
+    val title: String,
+    val position: Int,
+    val isPopular: Boolean,
+)
+
+@Composable
+fun TrackRow(
+    track: Track,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier
+            .height(52.dp)
+            .fillMaxWidth(), verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(Modifier.width(20.dp), contentAlignment = Alignment.Center) {
+            if (track.isPopular) {
+                Image(
+                    painterResource(id = R.drawable.ic_lightning),
+                    contentDescription = null,
+                    Modifier
+                        .padding(start = 4.dp)
+                        .size(8.dp, 12.dp),
+                    colorFilter = ColorFilter.tint(MaterialTheme.colors.onBackground),
+                    alpha = ContentAlpha.medium,
+                )
+            }
+        }
+
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Text(
+                track.position.toString(),
+                Modifier.size(16.dp),
+                fontSize = 12.sp,
+                textAlign = TextAlign.Center
+            )
+        }
+        Spacer(Modifier.width(8.dp))
+
+        Text(
+            track.title,
+            Modifier.weight(1f),
+        )
+
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                Icons.Filled.MoreVert,
+                contentDescription = null,
+                tint = MaterialTheme.colors.onBackground.copy(alpha = ContentAlpha.medium)
+            )
+        }
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview
+@Composable
+private fun TrackPreview() {
+    MusicAppComposeTheme {
+        Surface(color = MaterialTheme.colors.background) {
+            val track = Track("Boooubaou", 3, true)
+            TrackRow(track, Modifier.width(320.dp))
+        }
+    }
+}
